a3936faa6367d14c273e7b15b52d44b2eeda7feb
Kaio Colares <148463095+kaiocolares@users.noreply.github.com>
2025-02-09 18:16:38 +0000

    adicionando página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Ana, o futuro é construído pelas decisões de agora, só continuarmos na nossa jornada que chegaremos a nossos objetivos ! Te amo</h1>
+}
+
+export default Home;
