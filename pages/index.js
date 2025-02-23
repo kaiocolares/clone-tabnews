aee85cf82b9369ac617d8b8b94f1a871c6f6128f
Kaio Colares <148463095+kaiocolares@users.noreply.github.com>
2025-02-23 14:34:54 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Ana, o futuro é construído pelas decisões de agora, só continuarmos na nossa jornada que chegaremos a nossos objetivos ! Te amo</h1>
+  return (
+    <h1>
+      Ana, o futuro é construído pelas decisões de agora, só continuarmos na
+      nossa jornada que chegaremos a nossos objetivos ! Te amo
+    </h1>
+  );
 }
 
 export default Home;
